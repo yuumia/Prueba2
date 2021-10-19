97f83a32c4cb693e75196dc84ee9078d981dc0a9
Lalo <yuulfarrown@hotmail.com>
2021-10-19 16:17:07 -0500

    Hola Django 4

--- PWEB/Django/Hola/Hola/urls.py
+++ PWEB/Django/Hola/Hola/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from django.contrib import admin
+from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('principal/',include('principal.urls'))
 ]
